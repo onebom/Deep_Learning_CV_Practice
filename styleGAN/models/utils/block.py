957d1097fef6202c28d73f2e9f1253f955ce2329
onebom <bomsister@gmail.com>
2023-09-08 10:00:27 +0900

    ml algorithm

--- styleGAN/models/utils/block.py
+++ styleGAN/models/utils/block.py
@@ -1,3 +1,140 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class GMapping(nn.Module):
+    
+    def __init__(self, latent_size=512, dlatent_size=512, dlatent_broadcast=None,
+                 mapping_layers=8, mapping_fmaps=512, mapping_lrmul=0.01, mapping_nonlinearity='lrelu',
+                 use_wscale=True, normalize_latents=True, **kwargs):
+        super().__init__()
+
+        self.latent_size = latent_size
+        self.mapping_fmaps = mapping_fmaps
+        self.dlatent_size = dlatent_size
+        self.dlatent_broadcast = dlatent_broadcast
+
+        # Activation function.
+        act, gain = {'relu': (torch.relu, np.sqrt(2)),
+                     'lrelu': (nn.LeakyReLU(negative_slope=0.2), np.sqrt(2))}[mapping_nonlinearity]
+
+        # Embed labels and concatenate them with latents.
+        # TODO
+
+        layers = []
+        # Normalize latents.
+        if normalize_latents:
+            layers.append(('pixel_norm', PixelNormLayer()))
+
+        # Mapping layers. (apply_bias?)
+        layers.append(('dense0', EqualizedLinear(self.latent_size, self.mapping_fmaps,
+                                                 gain=gain, lrmul=mapping_lrmul, use_wscale=use_wscale)))
+        layers.append(('dense0_act', act))
+        for layer_idx in range(1, mapping_layers):
+            fmaps_in = self.mapping_fmaps
+            fmaps_out = self.dlatent_size if layer_idx == mapping_layers - 1 else self.mapping_fmaps
+            layers.append(
+                ('dense{:d}'.format(layer_idx),
+                 EqualizedLinear(fmaps_in, fmaps_out, gain=gain, lrmul=mapping_lrmul, use_wscale=use_wscale)))
+            layers.append(('dense{:d}_act'.format(layer_idx), act))
+
+        # Output.
+        self.map = nn.Sequential(OrderedDict(layers))
+
+    def forward(self, x):
+        # First input: Latent vectors (Z) [mini_batch, latent_size].
+        x = self.map(x)
+
+        # Broadcast -> batch_size * dlatent_broadcast * dlatent_size
+        if self.dlatent_broadcast is not None:
+            x = x.unsqueeze(1).expand(-1, self.dlatent_broadcast, -1)
+        return x
+
+class GSynthesis(nn.Module):
+    def __init__(self, dlatent_size=512, num_channels=3, resolution=1024,
+                 fmap_base=8192, fmap_decay=1.0, fmap_max=512,
+                 use_styles=True, const_input_layer=True, use_noise=True, nonlinearity='lrelu',
+                 use_wscale=True, use_pixel_norm=False, use_instance_norm=True, blur_filter=None,
+                 structure='linear', **kwargs):
+
+        super().__init__()
+
+        # if blur_filter is None:
+        #     blur_filter = [1, 2, 1]
+
+        def nf(stage):
+            return min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
+
+        self.structure = structure
+
+        resolution_log2 = int(np.log2(resolution))
+        assert resolution == 2 ** resolution_log2 and resolution >= 4
+        self.depth = resolution_log2 - 1
+
+        self.num_layers = resolution_log2 * 2 - 2
+        self.num_styles = self.num_layers if use_styles else 1
+
+        act, gain = {'relu': (torch.relu, np.sqrt(2)),
+                     'lrelu': (nn.LeakyReLU(negative_slope=0.2), np.sqrt(2))}[nonlinearity]
+
+        # Early layers.
+        self.init_block = InputBlock(nf(1), dlatent_size, const_input_layer, gain, use_wscale,
+                                     use_noise, use_pixel_norm, use_instance_norm, use_styles, act)
+        # create the ToRGB layers for various outputs
+        rgb_converters = [EqualizedConv2d(nf(1), num_channels, 1, gain=1, use_wscale=use_wscale)]
+
+        # Building blocks for remaining layers.
+        blocks = []
+        for res in range(3, resolution_log2 + 1):
+            last_channels = nf(res - 2)
+            channels = nf(res - 1)
+            # name = '{s}x{s}'.format(s=2 ** res)
+            blocks.append(GSynthesisBlock(last_channels, channels, blur_filter, dlatent_size, gain, use_wscale,
+                                          use_noise, use_pixel_norm, use_instance_norm, use_styles, act))
+            rgb_converters.append(EqualizedConv2d(channels, num_channels, 1, gain=1, use_wscale=use_wscale))
+
+        self.blocks = nn.ModuleList(blocks)
+        self.to_rgb = nn.ModuleList(rgb_converters)
+
+        # register the temporary upsampler
+        self.temporaryUpsampler = lambda x: interpolate(x, scale_factor=2)
+
+    def forward(self, dlatents_in, depth=0, alpha=0., labels_in=None):
+        """
+            forward pass of the Generator
+            :param dlatents_in: Input: Disentangled latents (W) [mini_batch, num_layers, dlatent_size].
+            :param labels_in:
+            :param depth: current depth from where output is required
+            :param alpha: value of alpha for fade-in effect
+            :return: y => output
+        """
+
+        assert depth < self.depth, "Requested output depth cannot be produced"
+
+        if self.structure == 'fixed':
+            x = self.init_block(dlatents_in[:, 0:2])
+            for i, block in enumerate(self.blocks):
+                x = block(x, dlatents_in[:, 2 * (i + 1):2 * (i + 2)])
+            images_out = self.to_rgb[-1](x)
+        elif self.structure == 'linear':
+            x = self.init_block(dlatents_in[:, 0:2])
+
+            if depth > 0:
+                for i, block in enumerate(self.blocks[:depth - 1]):
+                    x = block(x, dlatents_in[:, 2 * (i + 1):2 * (i + 2)])
+
+                residual = self.to_rgb[depth - 1](self.temporaryUpsampler(x))
+                straight = self.to_rgb[depth](self.blocks[depth - 1](x, dlatents_in[:, 2 * depth:2 * (depth + 1)]))
+
+                images_out = (alpha * straight) + ((1 - alpha) * residual)
+            else:
+                images_out = self.to_rgb[0](x)
+        else:
+            raise KeyError("Unknown structure: ", self.structure)
+
+        return images_out
+
+
 class Truncation(nn.Module):
     def __init__(self, avg_latent, max_layer=8, threshold=0.7, beta=0.995):
         super().__init__()
@@ -14,6 +151,7 @@ class Truncation(nn.Module):
         interp = torch.lerp(self.avg_latent, x, self.threshold)
         do_trunc = (torch.arange(x.size(1)) < self.max_layer).view(1, -1, 1).to(x.device)
         return torch.where(do_trunc, interp, x)
+
 
 # DiscriminatorBlock
 # DiscriminatorTop
